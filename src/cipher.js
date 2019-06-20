cd4f99c1f4d67831ff6fe0d56208c4e3f20b68e6
katiaram <51386374+katiaram@users.noreply.github.com>
2019-06-20 12:11:41 -0500

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,13 @@
 window.cipher = {
-  // ... 
+
+encode = (str, offset) => {
+  str = str.toUpperCase();
+  let encryptedStr = "";
+  let encryptedChar = "";
+  for (let i = 0; i < str.length; i++) {
+    let charASCII = str.charCodeAt(i);
+    encryptedChar = ((charASCII - 65 + offset) % 26) + 65;
+    encryptedStr = encryptedStr + String.fromCharCode(encryptedChar);
+  }
+  return encryptedStr;
 };
